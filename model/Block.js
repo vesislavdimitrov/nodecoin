74872320e523455ed4ce180fcb24df5e964faac3
vesislavdimitrov <vesislavdimitrov@gmail.com>
2024-01-21 19:45:32 +0200

    model

---
+++ model/Block.js
@@ -0,0 +1,46 @@
+import SHA256 from 'crypto-js/sha256.js';
+
+class Block {
+    
+    #index;
+    #timestamp;
+    #data;
+    #previousHash;
+    #hash;
+    #nonce;
+
+    /**
+     * @param {unix timestamp} timestamp 
+     * @param {*} data 
+     */
+    constructor(timestamp, data) {
+        this.#index = 0;
+        this.#timestamp = timestamp;
+        this.#data = data;
+        this.#previousHash = "0";
+        this.#hash = this.calculateHash();
+        this.#nonce = 0;
+    }
+
+    getIndex() { return this.#index }
+    getTimestamp() { return this.#timestamp }
+    getData() { return this.#data }
+    getPreviousHash() { return this.#previousHash }
+    getHash() { return this.#hash }
+    getNonce() { return this.#nonce }
+
+    setIndex(index) { this.#index = index }
+    setPreviousHash(previousHash) { this.#previousHash = previousHash }
+    setHash(hash) { this.#hash = hash }
+
+    calculateHash() {
+        return SHA256(
+            this.#index 
+            + this.#previousHash 
+            + this.#timestamp 
+            + this.#data 
+        ).toString();
+    }
+}
+
+export default Block;
---
+++ model/Blockchain.js
@@ -0,0 +1,73 @@
+import Block from './Block.js';
+
+const GENESIS_DATA = "I am the first one here!";
+
+class Blockchain {
+    
+    /**
+     * @private {Array}
+     */ 
+    #chain;
+
+    constructor() {
+        this.#chain = [this.#createGenesis()];
+    }
+
+    getChain() { return this.#chain }
+    
+    getChainContent() {
+        return this.#chain.map(block => ({
+            index: block.getIndex(),
+            timestamp: block.getTimestamp(),
+            data: block.getData(),
+            previousHash: block.getPreviousHash(),
+            hash: block.getHash(),
+            nonce: block.getNonce(),
+        }));
+    }
+
+    /**
+     * Adds a new block to the blockchain.
+     * @param {Block}
+     * @returns {void}
+     * Increments the index of the block, attaches the previous hash,
+     * recalculates its own hash, and pushes it to the chain list.
+     */
+    addBlock(block) {
+        block.setIndex(this.#getLatestBlock().getIndex() + 1);
+        block.setPreviousHash(this.#getLatestBlock().getHash());
+        block.setHash(block.calculateHash());
+        this.#chain.push(block);
+    }
+
+    /**
+     * Checks the validity of the blockchain.
+     * @returns {boolean}
+     * Iterates through the blocks starting from the second block, and checks
+     * if each block's hash matches the recalculated hash and if the previous
+     * hash matches the hash of the previous block in the chain.
+    */    
+    isValid() {
+        return this.#chain.slice(1).every((block, i) =>
+            block.getHash() === block.calculateHash() &&
+            block.getPreviousHash() === this.#chain[i].getHash()
+        );
+    }
+
+    /**
+     * @private
+     */
+    #getLatestBlock() {
+        return this.#chain[this.#chain.length - 1]
+    }
+
+    #createGenesis() {
+        return new Block(this.#createGenesisDate(), GENESIS_DATA)
+    }
+
+    #createGenesisDate() {
+        return Math.round(new Date().getTime() / 1000);
+    }
+}
+
+export default Blockchain;
